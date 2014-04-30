47a46a30cddb2e1e284d3077c5c3f0185eb62899
sw4 <samuel.wheeler@gmail.com>
2014-04-30 12:09:19 +0100

    Update codeFiddle.js

--- codeFiddle.js
+++ codeFiddle.js
@@ -1,18 +1,31 @@
 $(document).ready(function () {
     return codeFiddle={
-        run:function(cfEl, cssEditor, htmlEditor, jsEditor){                
-            var cfView = cfEl.find('.cf-view')[0];          
+        run:function(cfEl, cssEditor, htmlEditor, jsEditor, options){
+            var cfView = cfEl.find('.cf-view')[0],
+                stylesheets = '',
+                scripts = '';   
+            if(options && options.stylesheets){
+                options.stylesheets.forEach(function(href){
+                    stylesheets+='<link href="'+href+'" rel="stylesheet">';
+                });
+            }    
+            if(options && options.scripts){
+                options.scripts.forEach(function(src){
+                    scripts+='<script src="'+src+'"></script>';
+                });
+            }              
             cfView = (cfView.contentWindow) ? cfView.contentWindow : (cfView.contentDocument.document) ? cfView.contentDocument.document : cfView.contentDocument;
             cfView.document.open();
-            cfView.document.write('<!doctype html><html lang="en"><head><meta charset="utf-8"><style>'+cssEditor.getValue()+'</style></head><body>'+htmlEditor.getValue()+'<scr'+'ipt>'+jsEditor.getValue()+'</scr'+'ipt></body></html>');
+            cfView.document.write('<!doctype html><html lang="en"><head><meta charset="utf-8">'+stylesheets+'<style>'+cssEditor.getValue()+'</style></head><body>'+htmlEditor.getValue()+scripts+'<scr'+'ipt>'+jsEditor.getValue()+'</scr'+'ipt></body></html>');
             cfView.document.close();
         },
-        initCodeMirror:function(el){
-            return CodeMirror.fromTextArea(el[0], {
+        initCodeMirror:function(el, options){
+            options=options || {
                 lineNumbers: true,
                 mode: el.parent().hasClass('cf-html') ? "htmlmixed" : (el.parent().hasClass('cf-css') ? "css": "javascript"),
                 viewportMargin:Infinity
-            });            
+            };
+            return CodeMirror.fromTextArea(el[0], options);            
         },
         ctrl:false,
         template: "<div class='codeFiddle'>"+
@@ -24,25 +37,25 @@ $(document).ready(function () {
                     "</div>"+
                     "<button class='cf-run'>Output (Click / Ctrl+Enter to Update)</button>"+
                 "</div>",                 
-        init:function(options){
+        init:function(options){        
             var scope=this;                
             $('.codeFiddle').each(function(){                
                 var cfEl=$(this);
                 cfEl.html(scope.template);                                                        
-                var cssEditor=scope.initCodeMirror(cfEl.find('.cf-css textarea')),
-                    htmlEditor=scope.initCodeMirror(cfEl.find('.cf-html textarea')),
-                    jsEditor=scope.initCodeMirror(cfEl.find('.cf-js textarea'));
-                cfEl.find('.cf-run').on('click', function(){scope.run(cfEl, cssEditor, htmlEditor, jsEditor);});
+                var cssEditor=scope.initCodeMirror(cfEl.find('.cf-css textarea'), options && options.css && options.css.CodeMirror),
+                    htmlEditor=scope.initCodeMirror(cfEl.find('.cf-html textarea', options && options.html && options.html.CodeMirror)),
+                    jsEditor=scope.initCodeMirror(cfEl.find('.cf-js textarea', options && options.js && options.js.CodeMirror));
+                cfEl.find('.cf-run').on('click', function(){scope.run(cfEl, cssEditor, htmlEditor, jsEditor, options);});
                 cfEl.find('textarea').on('keydown keyup', function(e){                    
                     if(e.which===17 && e.type==="keydown"){
                         scope.ctrl=true;
                     }else if(e.which===13 && scope.ctrl){                            
-                        scope.run(cfEl, cssEditor, htmlEditor, jsEditor);
+                        scope.run(cfEl, cssEditor, htmlEditor, jsEditor, options);
                     }else if(e.which===17 && e.type==="keyup"){
                         scope.ctrl=false;
                     }
                 });
             });
         }        
-    };        
+    };
 });
